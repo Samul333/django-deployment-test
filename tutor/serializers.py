c407fb1fcc2b405a2e266ec623e4cb9458c18da9
Samul333 <samulshrestha101@gmail.com>
2021-04-21 22:12:46 +0545

    added the forget password code

--- tutor/serializers.py
+++ tutor/serializers.py
@@ -2,7 +2,17 @@ from rest_framework import serializers
 from .models import User,Subject,Ratings,Sessions,Bill
 from django.contrib import auth
 from rest_framework.exceptions import AuthenticationFailed
-
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_str, force_str,smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode    
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse 
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_str, force_str,smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode    
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse 
+from .utils import Util
 class RegisterSerializer(serializers.ModelSerializer):
     password=  serializers.CharField(max_length=68, min_length=6, write_only=True)
 
@@ -127,3 +137,37 @@ class BillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bill
         fields='__all__'
+
+
+
+class RequestPasswordEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    class Meta:
+        fields =['email']
+
+
+class SetNewPasswordSerialzier(serializers.Serializer):
+    password= serializers.CharField(min_length=6,write_only=True)
+    token= serializers.CharField(min_length=1,write_only=True)
+    uidb64= serializers.CharField(min_length=1,write_only=True)
+
+    class Meta:
+        fields=['password','token','uidb64']
+
+    def validate(self,attrs):
+        try:
+            password = attrs.get('password')
+            token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=id)
+
+            user.set_password(password)
+            user.save()
+            return (user)
+        except Exception as e:
+            raise AuthenticationFailed('Invalid token')
+
+        return super().validate(attrs)
